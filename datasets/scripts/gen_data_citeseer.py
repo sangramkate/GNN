a5f55a3190fb4ba807486aff81b7e08f4193158f
Akshi Tomar <atomar@login1.longhorn.tacc.utexas.edu>
2021-05-11 22:43:53 -0500

    adding new dataset and updated cost function

---
+++ datasets/scripts/gen_data_citeseer.py
@@ -0,0 +1,119 @@
+#!usr/bin/python
+
+import re
+import os
+
+citeseer_content = open("../citeseer/citeseer.content","r")
+citeseer_cites = open("../citeseer/citeseer.cites","r")
+
+label_name = ['Agents',
+              'AI',
+              'DB',
+              'IR',
+              'ML',
+              'HCI']
+
+label_value = [[1,0,0,0,0,0],
+               [0,1,0,0,0,0],
+               [0,0,1,0,0,0],
+               [0,0,0,1,0,0],
+               [0,0,0,0,1,0],
+               [0,0,0,0,0,1],
+               ]
+
+
+data_hash = {}
+node_count_map = {}
+count = 0
+for line in citeseer_content:
+    content = line.split("\t")
+    node = str(content[0])
+    data_hash[node] = {}
+    node_count_map[node] = count
+    features = content[1:3704]
+    data_hash[node]["features"] = features
+    label = re.match("(.*)\n",content[3704]).group(1)
+    label_index = label_name.index(label) 
+    data_hash[node]["label"] = label_value[label_index]
+    data_hash[node]["edges"] = [int(count)] #added self loop
+    count = count + 1
+
+citeseer_content.close()
+print(count)
+
+edge = 0
+for line in citeseer_cites:
+    cite = line.split("\t")
+    cite[1] = re.match("(.*)\n",cite[1]).group(1)
+    if cite[0] in data_hash:
+        if cite[1] in node_count_map:
+            data_hash[cite[0]]["edges"].append(int(node_count_map[cite[1]])) if int(node_count_map[cite[1]]) not in data_hash[cite[0]]["edges"] else data_hash[cite[0]]["edges"]
+    if cite[1] in data_hash:
+        if cite[0] in node_count_map:
+            data_hash[cite[1]]["edges"].append(int(node_count_map[cite[0]])) if int(node_count_map[cite[0]]) not in data_hash[cite[1]]["edges"] else data_hash[cite[1]]["edges"]
+    edge = edge + 1
+
+print(edge)
+
+
+
+#print(data_hash)
+#print(node_count_map)
+#nnz_data = open("../citeseer/nnz_data.csv","w")
+#nnz_count = edge*2+count
+#for i in range(0,nnz_count):
+#    nnz_data.write(str(1))
+#    if(i != nnz_count - 1):
+#        nnz_data.write(",")
+#nnz_data.close()
+
+row_start = open("../citeseer/row_start.csv","w")
+edge_dst = open("../citeseer/edge_dst.csv","w")
+edge_data = open("../citeseer/edge_data.csv", "w") #degree matrix inverse
+count = 0
+row_start.write(str(0))
+for node_count in sorted(node_count_map.items(), key=lambda x: x[1]):
+    count = count + len(data_hash[node_count[0]]["edges"]) 
+    degree = 1.0/len(data_hash[node_count[0]]["edges"])
+    row_start.write("," + str(count))
+    #print(node_count[0] is None)
+    data_hash[node_count[0]]["edges"].sort()
+    #print(data_hash[node_count[0]]["edges"])
+    for num in data_hash[node_count[0]]["edges"]:
+        edge_dst.write(str(num) + ",")
+        edge_data.write(str(degree) + ",")
+
+edge_dst.close()
+row_start.close()
+edge_data.close()
+
+with open("../citeseer/edge_dst.csv", 'rb+') as filehandle:
+    filehandle.seek(-1, os.SEEK_END)
+    filehandle.truncate()
+
+with open("../citeseer/edge_data.csv", 'rb+') as filehandle:
+    filehandle.seek(-1, os.SEEK_END)
+    filehandle.truncate()
+
+feat_val = open("../citeseer/feature_val.csv","w")
+label_val = open("../citeseer/label_val.csv","w")
+for node_count in sorted(node_count_map.items(), key=lambda x: x[1]):
+    feat_count = len(data_hash[node_count[0]]["features"])
+    #print(feat_count);
+    for feat in range(0,feat_count):
+        feat_val.write(str(data_hash[node_count[0]]["features"][feat]))
+        if (feat != feat_count - 1):
+            feat_val.write(",")
+    feat_val.write("\n")
+
+    label_count = len(data_hash[node_count[0]]["label"])
+    for lab in range(0,label_count):
+        label_val.write(str(data_hash[node_count[0]]["label"][lab]))
+        if(lab != label_count - 1):
+            label_val.write(",")
+    label_val.write("\n")
+
+feat_val.close()
+label_val.close()
+
+#print(data_hash)
---
+++ datasets/scripts/gen_data_pubmed.py
@@ -0,0 +1,119 @@
+#!usr/bin/python
+
+import os
+import re
+
+features = {}
+
+pubmed_content = open("../Pubmed-Diabetes/data/Pubmed-Diabetes.NODE.paper.tab","r")
+pubmed_cites = open("../Pubmed-Diabetes/data/Pubmed-Diabetes.DIRECTED.cites.tab")
+
+count = 0
+for line in pubmed_content:
+    if (re.match(".*summary=(.*)",line)):
+        feat = re.match(".*summary=(.*)\n",line).group(1)
+        feat_arr = feat.split(',')
+        for key in feat_arr:
+            if key not in features:
+                features[key] = count 
+                count = count + 1
+
+
+print(count)
+pubmed_content.close()
+
+#print(features)
+pubmed_content = open("../Pubmed-Diabetes/data/Pubmed-Diabetes.NODE.paper.tab","r")
+
+data_hash = {}
+node_count_map = {}
+count = 0
+for line in pubmed_content:
+    if (re.match(".*label=.*",line)):
+        content = line.split()
+        node = str(content[0])
+        data_hash[node] = {}
+        node_count_map[node] = count
+        #print(content)
+        if(content[1] == "label=1"):
+            data_hash[node]["label"] = [1,0,0]
+        elif (content[1] == "label=2"):
+            data_hash[node]["label"] = [0,1,0]
+        elif (content[1] == "label=3"):
+            data_hash[node]["label"] = [0,0,1]
+        data_hash[node]["features"] = [0] * 500
+        #print(content[-1])
+        features_arr = content[-1].split('=')
+        if (features_arr[0] == "summary"):
+            for feat in features_arr[1].split(','):
+                data_hash[node]["features"][features[feat]] = 1
+        data_hash[node]["edges"] = [int(count)] #added self loop
+        count = count + 1
+
+pubmed_content.close()
+print(count)
+
+edge = 0
+for line in pubmed_cites:
+    if (re.match(".*paper:(\d*).*paper:(\d*)",line)):
+        cite = ["",""]
+        cite[0] = re.match(".*paper:(\d*).*paper:(\d*)",line).group(1)
+        cite[1] = re.match(".*paper:(\d*).*paper:(\d*)",line).group(2)
+        data_hash[cite[0]]["edges"].append(int(node_count_map[cite[1]]))
+        data_hash[cite[1]]["edges"].append(int(node_count_map[cite[0]]))
+        edge = edge + 1
+
+print(edge)
+
+row_start = open("../Pubmed-Diabetes/row_start.csv","w")
+edge_dst = open("../Pubmed-Diabetes/edge_dst.csv","w")
+edge_data = open("../Pubmed-Diabetes/edge_data.csv", "w") #degree matrix inverse
+count = 0
+row_start.write(str(0))
+for node_count in sorted(node_count_map.items(), key=lambda x: x[1]):
+    count = count + len(data_hash[node_count[0]]["edges"]) 
+    degree = 1.0/len(data_hash[node_count[0]]["edges"])
+    row_start.write("," + str(count))
+    #print(node_count[0] is None)
+    data_hash[node_count[0]]["edges"].sort()
+    #print(data_hash[node_count[0]]["edges"])
+    for num in data_hash[node_count[0]]["edges"]:
+        edge_dst.write(str(num) + ",")
+        edge_data.write(str(degree) + ",")
+
+edge_dst.close()
+row_start.close()
+edge_data.close()
+
+with open("../Pubmed-Diabetes/edge_dst.csv", 'rb+') as filehandle:
+    filehandle.seek(-1, os.SEEK_END)
+    filehandle.truncate()
+
+with open("../Pubmed-Diabetes/edge_data.csv", 'rb+') as filehandle:
+    filehandle.seek(-1, os.SEEK_END)
+    filehandle.truncate()
+
+feat_val = open("../Pubmed-Diabetes/feature_val.csv","w")
+label_val = open("../Pubmed-Diabetes/label_val.csv","w")
+for node_count in sorted(node_count_map.items(), key=lambda x: x[1]):
+    feat_count = len(data_hash[node_count[0]]["features"])
+    #print(feat_count);
+    for feat in range(0,feat_count):
+        feat_val.write(str(data_hash[node_count[0]]["features"][feat]))
+        if (feat != feat_count - 1):
+            feat_val.write(",")
+    feat_val.write("\n")
+
+    if "label" not in data_hash[node_count[0]]:
+        print(node_count[0])
+    label_count = len(data_hash[node_count[0]]["label"])
+    for lab in range(0,label_count):
+        label_val.write(str(data_hash[node_count[0]]["label"][lab]))
+        if(lab != label_count - 1):
+            label_val.write(",")
+    label_val.write("\n")
+
+feat_val.close()
+label_val.close()
+
+#print(data_hash)
